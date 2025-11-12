48645111e4b293faea3daea49477215107c5e999
Egor Arkhiptsev <e.arkhiptsev@gmail.com>
2025-11-12 03:52:20 +0300

    Убедиться что URL commissions зарегистрирован

--- api-django/apps/affiliates/client_urls.py
+++ api-django/apps/affiliates/client_urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .client_views import (
     ClientReferralsView,
     ClientReferralPayoutsView,
-    ClientRequestPayoutView
+    ClientRequestPayoutView,
+    ClientReferralCommissionsView
 )
 
 urlpatterns = [
     path('referrals/', ClientReferralsView.as_view(), name='client-referrals'),
     path('referrals/payouts/', ClientReferralPayoutsView.as_view(), name='client-referral-payouts'),
+    path('referrals/commissions/', ClientReferralCommissionsView.as_view(), name='client-referral-commissions'),
     path('referrals/request-payout/', ClientRequestPayoutView.as_view(), name='client-request-payout'),
 ]
 
